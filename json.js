37508ace73944d875b76f4e1b022d81e6b7aa457
Kerim Kochekov <50640842+KerimKochekov@users.noreply.github.com>
2020-09-06 16:58:19 +0300

    Add files via upload

---
+++ json.js
@@ -0,0 +1,27 @@
+data = {"quizcontent": [
+    {"question": "1. Your plane touches down in Russia. What’s your next move?","image": "data/tmp1.jpg",  
+    "correct": 2, "a1":"Prepare your e-visa", "a2":"Applaud like you’re in a theater","a3":"Put your fur hat on because it’s cold",a4:"Don't know"},
+ 
+    {"question": "2.How much does a taxi ride from the airport to the center of Moscow cost?","image": "data/tmp2.jpg", 
+    "correct": 3, "a1":"Whatever the rate the taxi driver says", "a2":"Approximately 2,000 rubles","a3":"I’ll take a train instead and pay 500 rubles","a4":"Don't know"},
+ 
+    {"question": "3. What item would immediately reveal you are a foreigner?", "image": "data/tmp3.jpg",
+    "correct": 3, "a1":"A scarf as an accessory", "a2":"A sweater tied around your neck","a3":"Both items would","a4":"Don't know"},
+ 
+    {"question": "4. It’s January and -30C outside. Your Russian friends invite you out for a swim in an ice hole. Your move?","image": "data/tmp4.jpg",
+    "correct": 1, "a1":"They dive naked so I’ll have to think about it some more", "a2":"I’ll join them. I guess it’s safe if we wear thermal dive suits","a3":"It’s a prank and I won’t buy it!","a4":"Don't know"},
+ 
+    {"question": "5. You settled in Russia and bought a car. The speed limit in your city is 60 km/h, what speed would you adopt?", "image": "data/tmp5.jpg",
+    "correct": 2, "a1":"60 km/h", "a2":"79 km/h","a3":"From 40km/h to 59km/h","a4":"Don't know"},
+ 
+    {"question": "6. After some years you realize you have too much stuff you want to get rid off. Your next move?","image": "data/tmp6.jpg", 
+    "correct": 3, "a1":"Hold a garage sale or give it away to charity", "a2":"Throw it away","a3":"Put it onto your balcony and forget about it","a4":"Don't know"},
+ 
+    {"question": "7. If you choose to settle here, what drink you’d better get used to?", "image": "data/tmp7.jpg", 
+    "correct": 1, "a1":"Black tea", "a2":"Vodka","a3":"Café a La Russe","a4":"Don't know"},
+ 
+    {"question": "8. You get a flue. You next move?","image": "data/tmp8.jpg", 
+    "correct": 2, "a1":"Call an ambulance", "a2":"Buy pills at the nearest pharmacy and take them with hot tea","a3":"Visit a doctor","a4":"Don't know"}
+    ],
+	
+};
